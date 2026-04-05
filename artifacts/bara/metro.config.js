e0ae371a9891d589117f4be936b5607f2688c7d7
lexclusivegroupent-lgtm <lexclusivegroupent@gmail.com>
2026-04-05 13:07:15 +0200

    sync: artifacts/bara/metro.config.js

--- artifacts/bara/metro.config.js
+++ artifacts/bara/metro.config.js
@@ -1,16 +1,3 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
 
-const config = getDefaultConfig(__dirname);
-
-// Add workspace support for monorepo
-config.watchFolders = [
-  path.resolve(__dirname, "../.."),
-];
-
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, "../../node_modules"),
-  path.resolve(__dirname, "node_modules"),
-];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
